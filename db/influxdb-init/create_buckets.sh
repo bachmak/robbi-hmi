456d12b98c627900e4a97b7b09e6d8904245727e
Dmitrii Kochetov <dima.kochetov.70@gmail.com>
2026-03-23 18:36:58 +0100

    Multiple changes:
    - move common config variables to .env-file
    - add healthy check to start client only when db is ready
    - initialize buckets on db launch
    - fix auth token to make the whole config reproducible

---
+++ db/influxdb-init/create_buckets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+TOKEN="$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
+ORG="$DOCKER_INFLUXDB_INIT_ORG"
+
+BUCKETS=("$BUCKET_ROBOT" "$BUCKET_MISC")
+
+for bucket in "${BUCKETS[@]}"; do
+    echo "Creating bucket $bucket ..."
+    influx bucket create \
+        --name "$bucket" \
+        --org "$ORG" \
+        --token "$TOKEN"
+done
