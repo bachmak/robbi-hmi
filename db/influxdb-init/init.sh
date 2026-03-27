14c037adafe280fa785a9463704556fb444701aa
Dmitrii Kochetov <dima.kochetov.70@gmail.com>
2026-03-27 11:50:23 +0100

    Avoid overriding existing values on DB init

--- db/influxdb-init/init.sh
+++ db/influxdb-init/init.sh
@@ -3,20 +3,25 @@ set -e
 
 TOKEN="$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
 ORG="$DOCKER_INFLUXDB_INIT_ORG"
+BUCKET="$BUCKET_ROBOT"
 
-BUCKETS=("$BUCKET_ROBOT")
-
-for bucket in "${BUCKETS[@]}"; do
-    echo "Creating bucket $bucket ..."
+if influx bucket list \
+    --name "$BUCKET" \
+    --org "$ORG" \
+    --token "$TOKEN" \
+    --hide-headers | grep -q .; then
+    echo "Bucket $BUCKET already exists; skipping seed."
+else
+    echo "Creating bucket $BUCKET ..."
     influx bucket create \
-        --name "$bucket" \
+        --name "$BUCKET" \
         --org "$ORG" \
         --token "$TOKEN"
 
-    echo "Seeding zero motion_intent values into $bucket ..."
+    echo "Seeding zero motion_intent values into $BUCKET ..."
     influx write \
-        --bucket "$bucket" \
+        --bucket "$BUCKET" \
         --org "$ORG" \
         --token "$TOKEN" \
         "motion_intent v=0,omega=0,emergency_stop=false"
-done
+fi
